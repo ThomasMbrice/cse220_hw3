13033ede13d39e15ff3ab9174be8407631ec55c6
Thomas <145804162+ThomasMbrice@users.noreply.github.com>
2024-03-25 20:21:14 +0000

    include header

--- include/hw3.h
+++ include/hw3.h
@@ -1,12 +1,24 @@
 #include <stdlib.h>
 
-typedef struct GameState
-{
-    // define your struct here
+typedef struct ArrayofArrays {
+    char **array;
+    int **counterarray;
+    int rows;
+    int rowlen;
+} ArrayofArrays;
+
+typedef struct GameState{
+    ArrayofArrays *arr;
+    int currentindex;
 } GameState;
 
+int check_word(char *word);
 GameState* initialize_game_state(const char *filename);
 GameState* place_tiles(GameState *game, int row, int col, char direction, const char *tiles, int *num_tiles_placed);
 GameState* undo_place_tiles(GameState *game);
+GameState* gameextender(GameState *game);
+GameState* array_extender(GameState *game, int extend_num_of_rows_if_one, int inc_index);
 void free_game_state(GameState *game);
 void save_game_state(GameState *game, const char *filename);
+
+
